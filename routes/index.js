574942946d320c22daed7f7d631d5637f6012080
mori-kei <mojavascript13@gmail.com>
2024-02-21 14:49:47 +0900

    03まで終了した

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  const now = new Date();
+  res.render("index", { title: "Hello World", now: now });
+});
+router.get("/about", function (req, res, next) {
+  res.render("about", { title: "About" });
+});
+module.exports = router;
+
+router.get("/contact_form", function (req, res, next) {
+  res.render("contact_form", { title: "連絡先フォーム" });
 });
 
-module.exports = router;
+router.post("/contacts", function (req, res, next) {
+  console.log("posted", req.body);
+  res.redirect("/");
+});
---
+++ sandbox/list_contacts.js
@@ -0,0 +1,12 @@
+const models = require("../models");
+
+async function listContacts() {
+  const contacts = await models.Contact.findAll();
+  console.log(
+    contacts.map((contact) => {
+      return contact.name;
+    })
+  );
+}
+
+listContacts();
